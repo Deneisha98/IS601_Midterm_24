15c0e0b121a4f702cbc09743265d162ace6534cb
Deneisha98 <dmd46@njit.edu>
2024-10-24 15:53:21 -0400

    Fixed import paths in tests after project reorganization

---
+++ tests/test_plugins.py
@@ -0,0 +1,14 @@
+import calculator.plugins.sqrt as sqrt_plugin
+import calculator.plugins.power as power_plugin
+import calculator.plugins.log as log_plugin
+
+def test_sqrt():
+    assert sqrt_plugin.run(16) == 4
+
+def test_power():
+    assert power_plugin.run(2, 3) == 8
+
+def test_log():
+    assert log_plugin.run(100, 10) == 2
+
+
